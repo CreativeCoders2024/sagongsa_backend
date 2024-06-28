2b063c004cf6ded08be61ea4d8053a8cb6626ffc
KOO-DB <wkwns020609@naver.com>
2024-06-28 11:23:13 +0900

    Initial commit
    Co-authored-by: Jeehoo <pringle0531@naver.com>
    Co-authored-by: KOO-DB <wkwns020609@naver.com>
    Co-authored-by: syuh23 <seonghee5070@gmail.com>

---
+++ src/main/java/comflower/sagongsa/service/UserService.java
@@ -0,0 +1,64 @@
+package comflower.sagongsa.service;
+
+import comflower.sagongsa.entity.User;
+import comflower.sagongsa.dto.EditUserDTO;
+import comflower.sagongsa.dto.SignupDTO;
+import comflower.sagongsa.repository.UserRepository;
+import comflower.sagongsa.dto.LoginDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(SignupDTO signupDTO) {
+        validateDuplicateUser(signupDTO);
+        User signupUser = User.builder()
+                .id(signupDTO.getId())
+                .pw(signupDTO.getPw())
+                .nickname(signupDTO.getNickname())
+                .email((signupDTO.getEmail()))
+                .build();
+        userRepository.save(signupUser);
+    }
+
+    // 회원가입 - ID 중복 체크
+    private void validateDuplicateUser(SignupDTO signupDTO) {
+        userRepository.findById(signupDTO.getId())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("User with id : " + u.getId() + " already exists");
+                    //여기서 아예 500 에러로 떠버리는데 어떻게 에러 메세지를 보내지?
+                });
+    }
+
+    @Transactional
+    public String login(LoginDTO loginDTO) {
+        Optional<User> findUser = userRepository.findById(loginDTO.getId());
+        if (findUser.isPresent()) {
+            if(findUser.get().getPw().equals(loginDTO.getPw())) {
+                return "Success Login : " + findUser.get().getUserId();
+            }
+            else {
+                return "Wrong Password";
+            }
+        }
+        else {
+            return "User not found : " + loginDTO.getId();
+        }
+        // 이 리턴 처리를 service에서 하는게 맞나?
+    }
+
+    @Transactional
+    public void editUser(EditUserDTO editUserDTO) {
+        User editUser = userRepository.findByUserId(editUserDTO.getUserId()).orElseThrow(() ->
+                new IllegalStateException("User with id : " + editUserDTO.getUserId() + " not found"));
+        editUser.update(editUserDTO.getPw(), editUserDTO.getNickname());
+        userRepository.save(editUser);
+    }
+}
---
+++ src/test/java/comflower/sagongsa/SagongsaApplicationTests.java
@@ -0,0 +1,13 @@
+package comflower.sagongsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagongsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
