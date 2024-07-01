a3f3211a8443789472edc09b60434b34b9ce6191
syuh23 <seonghee5070@gmail.com>
2024-07-01 18:44:48 +0900

    start setting

---
+++ src/main/java/comflower/sagongsa/SagongsaApplication.java
@@ -0,0 +1,13 @@
+package comflower.sagongsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagongsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagongsaApplication.class, args);
+	}
+
+}
