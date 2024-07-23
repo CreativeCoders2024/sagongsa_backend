124917de50b6a82e34e36730bdc3a29e75aa5baa
Jeehoo <pringle0531@naver.com>
2024-07-23 22:52:13 +0900

    Fix formatting

--- src/main/java/comflower/sagongsa/repository/UserRepository.java
+++ src/main/java/comflower/sagongsa/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package comflower.sagongsa.repository;
+
 import comflower.sagongsa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
