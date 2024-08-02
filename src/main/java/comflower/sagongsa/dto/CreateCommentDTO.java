47cb57604e28adb4ff9cb1d3747069ec9785759f
Jeehoo <pringle0531@naver.com>
2024-08-02 14:02:22 +0900

    Reformat file

--- src/main/java/comflower/sagongsa/dto/CreateCommentDTO.java
+++ src/main/java/comflower/sagongsa/dto/CreateCommentDTO.java
@@ -1,13 +1,9 @@
 package comflower.sagongsa.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-
 public class CreateCommentDTO {
-
     private String content;
     private int parent;
-
 }
--- src/main/java/comflower/sagongsa/dto/EditCommentDTO.java
+++ src/main/java/comflower/sagongsa/dto/EditCommentDTO.java
@@ -1,7 +1,6 @@
 package comflower.sagongsa.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class EditCommentDTO {
