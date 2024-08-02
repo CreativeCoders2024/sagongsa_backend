63c1545a99ba9a907d30466e311a94e5e88c9427
지후 <165122252+imjeehoo@users.noreply.github.com>
2024-08-02 14:13:14 +0900

    Implement Comment  (#10)
    Co-authored-by: K_jajun <wkwns020609@naver.com>

---
+++ src/main/java/comflower/sagongsa/dto/EditCommentDTO.java
@@ -0,0 +1,8 @@
+package comflower.sagongsa.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EditCommentDTO {
+    private String content;
+}
---
+++ src/main/java/comflower/sagongsa/service/CommentService.java
@@ -0,0 +1,55 @@
+package comflower.sagongsa.service;
+
+import comflower.sagongsa.dto.CreateCommentDTO;
+import comflower.sagongsa.dto.EditCommentDTO;
+import comflower.sagongsa.entity.Comment;
+import comflower.sagongsa.repository.CommentRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public Comment createComment(Long postId, CreateCommentDTO createCommentDTO) {
+        throw new UnsupportedOperationException("로그인 화이팅");
+//        Comment comment = Comment.builder()
+//                .postId(postId)
+//                .userId(1L) // 임의로 설성 userId
+//                .content(createCommentDTO.getContent())
+//                .createdAt(LocalDateTime.now())
+//                .parentId(createCommentDTO.getParent())
+//                .build();
+//        commentRepository.save(comment);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("Contest with id : " + commentId + " not found"));
+        commentRepository.delete(comment);
+    }
+
+    @Transactional
+    public Comment editComment(Long commentId, EditCommentDTO editCommentDTO) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("Comment with id : " + commentId + " not found"));
+
+        comment.setContent(editCommentDTO.getContent());
+        comment.setEditedAt(LocalDateTime.now());
+
+        return commentRepository.save(comment);
+    }
+
+    @Transactional
+    public List<Comment> getComments(Long postId) {
+        return commentRepository.findByPostId(postId);
+    }
+}
