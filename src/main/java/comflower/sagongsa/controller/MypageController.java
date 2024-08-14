68508d35111dd4c0ea0cbcd6cfd251a53b2218c0
syuh23 <seonghee5070@gmail.com>
2024-08-14 17:53:33 +0900

    Edit mypage

--- src/main/java/comflower/sagongsa/controller/MypageController.java
+++ src/main/java/comflower/sagongsa/controller/MypageController.java
@@ -1,9 +1,15 @@
 package comflower.sagongsa.controller;
 
 
+import comflower.sagongsa.dto.request.EditIntroductionDTO;
 import comflower.sagongsa.dto.request.EditUserDTO;
+import comflower.sagongsa.dto.request.UserIdDTO;
+import comflower.sagongsa.dto.response.*;
 import comflower.sagongsa.entity.User;
+import comflower.sagongsa.error.UserNotFoundException;
+import comflower.sagongsa.repository.UserRepository;
 import comflower.sagongsa.service.MypageService;
+import comflower.sagongsa.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,40 +18,85 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController("/user")
+@RequestMapping("/user")
+@RestController
 @RequiredArgsConstructor
 public class MypageController {
     private final MypageService mypageService;
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException e) {
+        return ErrorResponse.entity(ErrorType.USER_NOT_FOUND, e.getUserId());
+    }
 
     // 소개글 수정을 위해 불러오는 로직
     @PostMapping("/import/intro")
-    public ResponseEntity<Optional<User>> importIntroduction(@RequestBody EditUserDTO editUserDTO) {
-        Optional<User> importingIntroduction_data = mypageService.importingIntroduction(editUserDTO);
-        return ResponseEntity.ok(importingIntroduction_data);
+    public MypageImportIntroductionResponse importIntroduction(@RequestBody UserIdDTO userIdDTO) {
+        User importIntroductionUser = mypageService.importingIntroduction(userIdDTO)
+                .orElseThrow(() -> new UserNotFoundException(userIdDTO.getUserId()));
+
+        String introduction = importIntroductionUser.getIntroduction();
+        if(introduction == null) {
+            introduction = "null";
+        }
+
+        MypageImportIntroductionResponse importIntro = MypageImportIntroductionResponse
+                .builder()
+                .introduction(introduction)
+                .build();
+        return importIntro;
     }
 
     // 소개글 작성 및 수정
     @PutMapping("/edit/intro")
-    public ResponseEntity<EditUserDTO> editUserIntroduction(@RequestBody EditUserDTO editUserDTO) {
-        mypageService.editUserIntroduction(editUserDTO);
-        return ResponseEntity.ok(editUserDTO);
+    public UserIdResponse editUserIntroduction(@RequestBody EditIntroductionDTO editIntroDTO) {
+        if(!userService.isUserPresentByUserId(editIntroDTO.getUserId())) {
+            throw new UserNotFoundException(editIntroDTO.getUserId());
+        }
+        User findEditUserIntro = userRepository.findByUserId(editIntroDTO.getUserId()).get();
+        mypageService.editUserIntroduction(findEditUserIntro, editIntroDTO);
+
+        UserIdResponse editintroResponse = UserIdResponse
+                .builder()
+                .userId(editIntroDTO.getUserId())
+                .build();
+
+        return editintroResponse;
     }
 
     // 관리자 권한 수정
-    @PutMapping("/manager")
-    public void editUserManager(@RequestBody EditUserDTO editUserDTO) {
+    @PutMapping("/edit/manager")
+    public UserIdResponse editUserManager(@RequestBody UserIdDTO editManagerDTO) {
+        User findEditUserManager = userRepository.findByUserId(editManagerDTO.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(editManagerDTO.getUserId()));
+        mypageService.editUserManager(findEditUserManager);
 
+        UserIdResponse editManagerResponse = UserIdResponse
+                .builder()
+                .userId(findEditUserManager.getUserId())
+                .build();
+        return editManagerResponse;
     }
 
     // 분야 불러오기
     @PostMapping("/import/field")
-    public void importField(@RequestBody EditUserDTO editUserDTO) {
+    public ImportFieldResponse importField(@RequestBody UserIdDTO userIdDTO) {
+        User findImportField = mypageService.importField(userIdDTO.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(userIdDTO.getUserId()));
 
+        ImportFieldResponse importField = ImportFieldResponse
+                .builder()
+                .field(findImportField.getField())
+                .build();
+
+        return importField;
     }
 
     // 분야 수정
     @PutMapping("/edit/field")
-    public void editField(@RequestBody EditUserDTO editUserDTO) {
+    public void editField(@RequestBody UserIdDTO editFieldDTO) {
 
     }
 }
---
+++ src/main/java/comflower/sagongsa/dto/response/ImportFieldResponse.java
@@ -0,0 +1,10 @@
+package comflower.sagongsa.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ImportFieldResponse {
+    private int field;
+}
