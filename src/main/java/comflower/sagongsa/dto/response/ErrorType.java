117855601e65ffbe007b74725fcb7a4c1fe09b0d
Jeehoo <pringle0531@naver.com>
2024-09-03 10:52:59 +0900

    Remove unused errors

--- src/main/java/comflower/sagongsa/dto/response/ErrorType.java
+++ src/main/java/comflower/sagongsa/dto/response/ErrorType.java
@@ -9,7 +9,6 @@ public enum ErrorType {
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST, 10001, "Wrong Password"),
     WRONG_ID(HttpStatus.BAD_REQUEST, 10002, "Wrong ID"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 10003, "User Not Found"),
-    ERROR_IN_PROCESSING(HttpStatus.CONFLICT, 10004, "Errors in processing"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, 10009, "Post not found"),
     INVALID_POST_DATA(HttpStatus.BAD_REQUEST, 10005, "Invalid post data"),
     UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, 10006, "Unauthorized access"),
@@ -17,9 +16,7 @@ public enum ErrorType {
     INVALID_COMMENT_DATA(HttpStatus.BAD_REQUEST, 10008, "Invalid comment data"),
     INVALID_CONTEST_DATA(HttpStatus.BAD_REQUEST, 10009, "Invalid contest data"),
     CONTEST_NOT_FOUND(HttpStatus.NOT_FOUND, 100010, "Contest not found"),
-    CONTEST_Edit_NOT_FOUND(HttpStatus.NOT_FOUND, 10011, "Contest not found for edit"),
-    INVALID_CONTEST_Edit_DATA(HttpStatus.BAD_REQUEST, 10012, "Invalid contest data for edit"),
-    CONTEST_DELETE_FAILED(HttpStatus.NOT_FOUND, 10013, "Failed to delete contest");
+    INVALID_CONTEST_EDIT_DATA(HttpStatus.BAD_REQUEST, 10011, "Invalid contest data for edit");
 
     private final HttpStatus httpStatus;
     private final int code;
