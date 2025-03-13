059691a7facf76527cb4208fbeda15e2e13204de
Jeehoo <pringle0531@naver.com>
2025-03-13 12:25:47 +0900

    Reorder

--- src/main/java/comflower/sagongsa/request/EditContestRequest.java
+++ src/main/java/comflower/sagongsa/request/EditContestRequest.java
@@ -1,8 +1,6 @@
 package comflower.sagongsa.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.validation.Errors;
@@ -13,27 +11,13 @@ import java.net.URI;
 @Builder
 @Schema(description = "대회 수정 DTO")
 public class EditContestRequest implements Request {
-    @NotBlank
-    @Schema(description = "대회 제목", example = "대회 제목")
     private String title;
-    @NotBlank
-    @Schema(description = "대회 이미지", example = "대회 이미지")
-    private String thumbnail;
-    @NotBlank
-    @Schema(description = "대회 상금", example = "대회 상금")
-    private String prize;
-    @NotNull
-    @Schema(description = "대회 시작 시간", example = "2024-01-01 00:00:00")
-    private long startedAt;
-    @NotNull
-    @Schema(description = "대회 종료 시간", example = "2024-01-01 00:00:00")
-    private long endedAt;
-    @NotBlank
-    @Schema(description = "대회 링크", example = "대회 링크")
     private String link;
-    @NotNull
-    @Schema(description = "대회 분야", example = "대회 분야")
+    private String prize;
     private int topic;
+    private long startedAt;
+    private long endedAt;
+    private String thumbnail;
 
     @Override
     public void validate(Errors errors) {
